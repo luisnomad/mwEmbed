e0cb575ad21ed46b2484a658ad54af90096b7d95
Michael Dale <mdale@wikimedia.org>
2013-07-10 13:54:31 -0500

    Peer5: fixed Peer5 check to not load library if not compatible

--- modules/Peer5/Peer5.loader.js
+++ modules/Peer5/Peer5.loader.js
@@ -1,16 +1,15 @@
 ( function( mw, $ ) { "use strict";
-	mw.addKalturaPlugin( ['mw.Peer5'], 'peer5', function( embedPlayer, callback){
-		var ua = navigator.userAgent;
-		if( /chrome/i.test(ua)) {
-			var uaArray = ua.split(' ');
-			// check chrome browser version: 
-			if( parseInt( uaArray[uaArray.length - 2].substr(7).split('.')[0] ) > 25 ){
+	var ua = navigator.userAgent;
+	if( /chrome/i.test(ua)) {
+		var uaArray = ua.split(' ');
+		// only add plugin if we pass ua checks chrome browser version: 
+		if( parseInt( uaArray[uaArray.length - 2].substr(7).split('.')[0] ) > 25 ){
+			mw.addKalturaPlugin( ['mw.Peer5'], 'peer5', function( embedPlayer, callback){
 				embedPlayer.peer5 = new mw.Peer5( embedPlayer, callback );
-				return ;
-			}
+				callback();
+			});
 		}
-		//if not HTML5 supported, should not get in!
-		//be sure to issue callback if not running plugin:
-		callback();
-	});
+	} else {
+		// Peer5 not loaded, incompatible user agent
+	}
 })( window.mw, jQuery );
