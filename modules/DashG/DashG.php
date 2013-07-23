6cf1733c77692320e790f8464c2224f2b271dbf0
Michael Dale <mdale@wikimedia.org>
2013-07-23 16:24:16 -0700

    DashG: stubs for google dash implementation

---
+++ modules/DashG/DashG.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+	'mw.DashG' => array( 
+		'scripts' => 'resources/mw.DashG.js',
+		'kalturaPluginName' => 'dashG',
+		'd'
+	),
+	'dash-eme' => array(
+		'scripts' => 'resources/dash-eme.js'
+	),
+	'dash-manifest' => array(
+		'scripts' => 'resources/dash-manifest.js'
+	),
+	'media-source-portability' => array(
+		'scripts' => 'resources/media-source-portability.js'
+	),
+	'player.js' => array(
+		'scripts' => 'resources/player.js'
+	),
+	'segmentindex' => array(
+		'scripts' => 'resources/segmentindex.js'
+	)
+);
